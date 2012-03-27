712fd76f3b5afc97e657fbdc58d5c8eb62bd3e8f
NicholasPufal <niko.pufal@gmail.com>
2012-03-27 10:39:10 -0300

    Starting to add groups.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 AlternativaTcc::Application.routes.draw do
   root :to => "exams#index"  
   resources :exams
+  resources :groups
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
