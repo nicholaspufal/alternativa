f46d87a3d7c8421b9480e57fcc6d0dc144c16917
NicholasPufal <niko.pufal@gmail.com>
2012-04-16 08:22:41 -0300

    ApplicationController is redirecting to admin or public area according to the user permission.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,10 @@ class ApplicationController < ActionController::Base
   
   respond_to :html
   
+  protected
+  
+  def after_sign_in_path_for(resource)
+    resource.admin? ? admin_root_path : root_path
+  end
+  
 end
