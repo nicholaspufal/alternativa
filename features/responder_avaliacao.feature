c544dff38a443c21a5a6a23b9c603daa1c095b0f
NicholasPufal <niko.pufal@gmail.com>
2012-05-19 15:45:13 -0300

    Added new acceptance tests.

--- features/responder_avaliacao.feature
+++ features/responder_avaliacao.feature
@@ -20,3 +20,23 @@ Cenário: Aluno gabarita uma avaliação
 	E que eu respondi toda ela corretamente
 	Quando eu clicar em "Finalizar avaliação"
 	Então eu devo ver uma mensagem de parabéns
+	
+Cenário: Aluno visualiza as respostas para questões erradas
+	Dado que eu entrei na avaliação "Minha primeira prova"
+	E que eu errei a primeira e a última questão
+	Quando eu clicar em "Finalizar avaliação"
+	Então eu devo ver as respostas certas para a primeira e a última questões
+	
+Cenário: Aluno visualiza as questões corretas
+	Dado que eu entrei na avaliação "Minha primeira prova"
+	E que eu errei a primeira e a última questão
+	Quando eu clicar em "Finalizar avaliação"
+	Então eu devo ver a segunda e a terceira questões listadas como corretas
+	
+Cenário: Aluno submete a avaliação sem responder nenhuma questão
+	Dado que eu entrei na avaliação "Minha primeira prova"
+	E que eu não marquei nenhuma resposta
+	Quando eu clicar em "Finalizar avaliação"
+	Então eu devo ver uma mensagem informando que ao menos uma questão deve ser respondida
+	
+	
--- features/step_definitions/student/responder_avaliacao_steps.rb
+++ features/step_definitions/student/responder_avaliacao_steps.rb
@@ -19,6 +19,16 @@ Dado /^que eu respondi toda ela corretamente$/ do
   choose("answers__3_resposta_certa")
 end
 
+Dado /^que eu errei a primeira e a última questão$/ do
+  choose("answers__0_alguma_resposta_errada")
+  choose("answers__1_resposta_certa")
+  choose("answers__2_resposta_certa")
+  choose("answers__3_alguma_resposta_errada")
+end
+
+Dado /^que eu não marquei nenhuma resposta$/ do
+end
+
 Quando /^eu clicar em "(.*)"$/ do |button_value|
   click_button button_value
 end
@@ -29,4 +39,16 @@ end
 
 Então /^eu devo ver uma mensagem de parabéns$/ do
   page.should have_content("Parabéns!")
+end
+
+Então /^eu devo ver as respostas certas para a primeira e a última questões$/ do
+  page.should have_css("table#wrong_questions tr", :count => 3)
+end
+
+Então /^eu devo ver a segunda e a terceira questões listadas como corretas$/ do
+  page.should have_css("table#correct_questions tr", :count => 3)
+end
+
+Então /^eu devo ver uma mensagem informando que ao menos uma questão deve ser respondida$/ do
+  page.should have_content("precisa responder ao menos uma questão")
 end
